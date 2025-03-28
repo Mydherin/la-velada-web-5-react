3a6b30c3e7689fd88ef2c6a9186af5d7d144afb3
Mydherin <mydherin@gmail.com>
2025-03-28 22:40:59 +0100

    chore: intialize project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <div className="h-full bg-velada-pink">
+      <footer className="relative h-[400px] flex justify-center">
+        <div className="absolute bg-[url('/images/footer.webp')] bg-cover bg-bottom h-full w-full" />
+        <div className="z-10 lowercase">
+          <p>2025 La Velada del Año. Todos los derechos reservados.</p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default App;
